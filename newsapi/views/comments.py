8cab144d740a62aea777e90b837f41b59b4b1637
Jordan VanVeldhuisen <jordanvanveldhuisen@gmail.com>
2024-04-29 11:04:46 -0400

    added the ability to edit and delete a comment

--- newsapi/views/comments.py
+++ newsapi/views/comments.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.db import IntegrityError
 from rest_framework.exceptions import ValidationError
+from rest_framework import permissions
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -13,9 +14,24 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit or delete it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed for any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed for the owner
+        return obj.user == request.user
+
+
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     # Custom action to filter comments by article URL
     def list(self, request):
@@ -40,3 +56,26 @@ class CommentViewSet(viewsets.ModelViewSet):
             )
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super().destroy(request, *args, **kwargs)
+        except Exception as ex:
+            return Response(
+                {"message": str(ex)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        # Check if the user is the owner of the comment
+        if instance.user != request.user:
+            return Response(
+                {"error": "You are not authorized to update this comment."},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        serializer.save()
+        return Response(serializer.data)
